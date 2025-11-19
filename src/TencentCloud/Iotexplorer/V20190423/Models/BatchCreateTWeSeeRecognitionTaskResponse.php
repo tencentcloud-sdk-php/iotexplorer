650c0e317f976c93d856359e3571c401d8557f5f
tencentcloudapi <tencentcloudapi@tencent.com>
2025-11-20 04:12:41 +0800

    release 3.0.1491

---
+++ src/TencentCloud/Iotexplorer/V20190423/Models/BatchCreateTWeSeeRecognitionTaskResponse.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Copyright (c) 2017-2025 Tencent. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace TencentCloud\Iotexplorer\V20190423\Models;
+use TencentCloud\Common\AbstractModel;
+
+/**
+ * BatchCreateTWeSeeRecognitionTask返回参数结构体
+ *
+ * @method array getOutputs() 获取TWeSee 语义理解任务的创建结果。与入参 Inputs 一一对应。
+ * @method void setOutputs(array $Outputs) 设置TWeSee 语义理解任务的创建结果。与入参 Inputs 一一对应。
+ * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+ * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+ */
+class BatchCreateTWeSeeRecognitionTaskResponse extends AbstractModel
+{
+    /**
+     * @var array TWeSee 语义理解任务的创建结果。与入参 Inputs 一一对应。
+     */
+    public $Outputs;
+
+    /**
+     * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+     */
+    public $RequestId;
+
+    /**
+     * @param array $Outputs TWeSee 语义理解任务的创建结果。与入参 Inputs 一一对应。
+     * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+     */
+    function __construct()
+    {
+
+    }
+
+    /**
+     * For internal only. DO NOT USE IT.
+     */
+    public function deserialize($param)
+    {
+        if ($param === null) {
+            return;
+        }
+        if (array_key_exists("Outputs",$param) and $param["Outputs"] !== null) {
+            $this->Outputs = [];
+            foreach ($param["Outputs"] as $key => $value){
+                $obj = new CreateVisionRecognitionTaskOutput();
+                $obj->deserialize($value);
+                array_push($this->Outputs, $obj);
+            }
+        }
+
+        if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
+            $this->RequestId = $param["RequestId"];
+        }
+    }
+}
