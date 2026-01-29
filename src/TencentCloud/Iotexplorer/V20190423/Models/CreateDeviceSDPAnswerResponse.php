551684f93b8fa85cd2c94b537e387197cf4c05a8
tencentcloudapi <tencentcloudapi@tencent.com>
2026-01-29 10:17:49 +0800

    release 3.0.1531

--- src/TencentCloud/Iotexplorer/V20190423/Models/CreateDeviceSDPAnswerResponse.php
+++ src/TencentCloud/Iotexplorer/V20190423/Models/CreateDeviceSDPAnswerResponse.php
@@ -18,19 +18,27 @@ namespace TencentCloud\Iotexplorer\V20190423\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * CancelAssignTWeCallLicense返回参数结构体
+ * CreateDeviceSDPAnswer返回参数结构体
  *
+ * @method string getSDPAnswer() 获取SDP应答
+ * @method void setSDPAnswer(string $SDPAnswer) 设置SDP应答
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
-class CancelAssignTWeCallLicenseResponse extends AbstractModel
+class CreateDeviceSDPAnswerResponse extends AbstractModel
 {
+    /**
+     * @var string SDP应答
+     */
+    public $SDPAnswer;
+
     /**
      * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     public $RequestId;
 
     /**
+     * @param string $SDPAnswer SDP应答
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -46,6 +54,10 @@ class CancelAssignTWeCallLicenseResponse extends AbstractModel
         if ($param === null) {
             return;
         }
+        if (array_key_exists("SDPAnswer",$param) and $param["SDPAnswer"] !== null) {
+            $this->SDPAnswer = $param["SDPAnswer"];
+        }
+
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
             $this->RequestId = $param["RequestId"];
         }
