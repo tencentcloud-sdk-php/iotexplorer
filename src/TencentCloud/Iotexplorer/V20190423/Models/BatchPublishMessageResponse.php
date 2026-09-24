a5f3990c309b7d66ca7a2fb1297039bb9d0932e9
tencentcloudapi <tencentcloudapi@tencent.com>
2026-09-25 04:27:47 +0800

    release 3.0.1674

--- src/TencentCloud/Iotexplorer/V20190423/Models/BatchPublishMessageResponse.php
+++ src/TencentCloud/Iotexplorer/V20190423/Models/BatchPublishMessageResponse.php
@@ -18,33 +18,33 @@ namespace TencentCloud\Iotexplorer\V20190423\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * DescribeCloudStorageEventsByTWeSeePerson返回参数结构体
+ * BatchPublishMessage返回参数结构体
  *
- * @method array getEvents() 获取<p>人员关联的云存事件列表</p>
- * @method void setEvents(array $Events) 设置<p>人员关联的云存事件列表</p>
- * @method integer getTotal() 获取<p>人员关联的云存事件总数</p>
- * @method void setTotal(integer $Total) 设置<p>人员关联的云存事件总数</p>
- * @method string getVideoURL() 获取<p>视频播放URL</p>
- * @method void setVideoURL(string $VideoURL) 设置<p>视频播放URL</p>
+ * @method integer getTotal() 获取<p>批量推送总数</p>
+ * @method void setTotal(integer $Total) 设置<p>批量推送总数</p>
+ * @method integer getSuccessCount() 获取<p>成功数量</p>
+ * @method void setSuccessCount(integer $SuccessCount) 设置<p>成功数量</p>
+ * @method array getFailures() 获取<p>失败明细</p>
+ * @method void setFailures(array $Failures) 设置<p>失败明细</p>
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
-class DescribeCloudStorageEventsByTWeSeePersonResponse extends AbstractModel
+class BatchPublishMessageResponse extends AbstractModel
 {
     /**
-     * @var array <p>人员关联的云存事件列表</p>
-     */
-    public $Events;
-
-    /**
-     * @var integer <p>人员关联的云存事件总数</p>
+     * @var integer <p>批量推送总数</p>
      */
     public $Total;
 
     /**
-     * @var string <p>视频播放URL</p>
+     * @var integer <p>成功数量</p>
      */
-    public $VideoURL;
+    public $SuccessCount;
+
+    /**
+     * @var array <p>失败明细</p>
+     */
+    public $Failures;
 
     /**
      * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
@@ -52,9 +52,9 @@ class DescribeCloudStorageEventsByTWeSeePersonResponse extends AbstractModel
     public $RequestId;
 
     /**
-     * @param array $Events <p>人员关联的云存事件列表</p>
-     * @param integer $Total <p>人员关联的云存事件总数</p>
-     * @param string $VideoURL <p>视频播放URL</p>
+     * @param integer $Total <p>批量推送总数</p>
+     * @param integer $SuccessCount <p>成功数量</p>
+     * @param array $Failures <p>失败明细</p>
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -70,21 +70,21 @@ class DescribeCloudStorageEventsByTWeSeePersonResponse extends AbstractModel
         if ($param === null) {
             return;
         }
-        if (array_key_exists("Events",$param) and $param["Events"] !== null) {
-            $this->Events = [];
-            foreach ($param["Events"] as $key => $value){
-                $obj = new CloudStorageEventWithAITasks();
-                $obj->deserialize($value);
-                array_push($this->Events, $obj);
-            }
-        }
-
         if (array_key_exists("Total",$param) and $param["Total"] !== null) {
             $this->Total = $param["Total"];
         }
 
-        if (array_key_exists("VideoURL",$param) and $param["VideoURL"] !== null) {
-            $this->VideoURL = $param["VideoURL"];
+        if (array_key_exists("SuccessCount",$param) and $param["SuccessCount"] !== null) {
+            $this->SuccessCount = $param["SuccessCount"];
+        }
+
+        if (array_key_exists("Failures",$param) and $param["Failures"] !== null) {
+            $this->Failures = [];
+            foreach ($param["Failures"] as $key => $value){
+                $obj = new DeviceResult();
+                $obj->deserialize($value);
+                array_push($this->Failures, $obj);
+            }
         }
 
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
